74ec0107827b35f9d9760f16c7660b555c5ff6d2
nfsboy <18615926+SanjaySasimandir@users.noreply.github.com>
2020-12-18 12:52:48 +0530

    Updated percentage function, code cleanup.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,13 +1,12 @@
 var inputBox = document.querySelector("input"); // Selects the input field
 inputBox.value = ""; // Clears the field when page is loaded
-inputBox.focus();
 
 var operations = ["÷", "×", "+", "−", "%"]; // Contains all the operators used in the calculations
 
 
 // Function which gets called when a number or operator button is clicked
 function passid(id) {
-    var clickedvalue = document.getElementById(id).innerText;
+    let clickedvalue = document.getElementById(id).innerText;
 
     if (clickedvalue == "." && inputBox.value[inputBox.value.length - 1] != ".") {
         inputBox.value += clickedvalue;
@@ -26,7 +25,6 @@ function symbolpressed(clickedvalue) {
 
     if (isNaN(inputBox.value[inputBox.value.length - 1])) {
         inputBox.value = inputBox.value.substring(0, inputBox.value.length - 1) + clickedvalue;
-        console.log('yes')
     }
     else {
         inputBox.value += clickedvalue;
@@ -43,9 +41,9 @@ function clearbox() {
 
 // When = button is clicked this function is called
 function caclulate() {
-    var fieldArray = formatInputField();
+    let fieldArray = formatInputField();
     inputBox.value = (process(fieldArray));
-    
+
     if (inputBox.value == Infinity) {
         inputBox.value = "Not Defined";
     }
@@ -54,9 +52,9 @@ function caclulate() {
 
 // Function converts input field text to an array with parsed numbers and operators
 function formatInputField() {
-    var field = inputBox.value;
-    var eval = [];
-    var temp = 0;
+    let field = inputBox.value;
+    let eval = [];
+    let temp = 0;
     for (let i = 0; i < field.length; i++) {
         let elem = field[i];
         if (!isNaN(elem)) {
@@ -92,22 +90,10 @@ function process(array) {
 // This function gets the index of an operator, removes the operator, the number before and after it, and then inserts the answer.
 function popAndInsert(index, answer, array) {
     let newArray = [];
-    newArray = array.slice(0, index-1);
+    newArray = array.slice(0, index - 1);
     newArray.push(answer);
-    newArray=newArray.concat(array.slice(index+2 , array.length));
-    
-    
-    /*
-    // newArray.push(answer);
-    // newArray = newArray.concat(array.slice(index - 3, array.length));
-    // console.log(newArray);
-    if (array.length > 3) {
-        newArray.push(answer);
-        newArray = newArray.concat(array.slice(index - 3, array.length));
-    }
-    else{
-        newArray=[answer];
-    }*/
+    newArray = newArray.concat(array.slice(index + 2, array.length));
+
     return newArray;
 }
 
@@ -144,7 +130,6 @@ function typingSymbolChange() {
         while (field.includes(item)) {
             let fieldIndex = field.indexOf(item);
             field = field.replace(item, newList[index]);
-            console.log(field);
         }
     })
     inputBox.value = field;
@@ -154,8 +139,18 @@ function backspace() {
     inputBox.value = inputBox.value.substring(0, inputBox.value.length - 1);
 }
 
+
+// Called when percentage button is clicked
 function percentage() {
-    var fieldArray = formatInputField();
-    var lastNumber=fieldArray[fieldArray.length-1];
-    inputBox.value=fieldArray.slice(0,fieldArray.length-1).join("")+process(fieldArray.slice(0,fieldArray.length-2))*lastNumber*0.01;
+    let fieldArray = formatInputField();
+    let lastNumber = fieldArray[fieldArray.length - 1];
+
+    // If only a single number is in the input and the percentage button is pressed, it returns 1% of the value
+    if (fieldArray.length < 3) {
+        inputBox.value = lastNumber * 0.01;
+    }
+    // Else, the intermediate answer till the last number is calculated, and the last number is replaced with the percentage of the intermediate answer.
+    else {
+        inputBox.value = fieldArray.slice(0, fieldArray.length - 1).join("") + process(fieldArray.slice(0, fieldArray.length - 2)) * lastNumber * 0.01;
+    }
 }
